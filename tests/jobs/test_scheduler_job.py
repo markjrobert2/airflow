apache/spark

[AIRFLOW-6576] Fix scheduler crash caused by deleted task with sla misses (#7187)
When a task with SLA is deleted from a DAG after the SLA miss is logged
but before the notification was sent, scheduler will crash with an
AirflowException

tests/jobs/test_scheduler_job.py
@@ -379,6 +379,37 @@ class TestDagFileProcessor(unittest.TestCase):
             'Could not send SLA Miss email notification for DAG %s',
             'test_sla_miss')
 
+    def test_dag_file_processor_sla_miss_deleted_task(self):
+        """
+        Test that the dag file processor will not crash when trying to send
+        sla miss notification for a deleted task
+        """
+        session = settings.Session()
+
+        test_start_date = days_ago(2)
+        dag = DAG(dag_id='test_sla_miss',
+                  default_args={'start_date': test_start_date,
+                                'sla': datetime.timedelta(days=1)})
+
+        task = DummyOperator(task_id='dummy',
+                             dag=dag,
+                             owner='airflow',
+                             email='test@test.com',
+                             sla=datetime.timedelta(hours=1))
+
+        session.merge(models.TaskInstance(task=task,
+                                          execution_date=test_start_date,
+                                          state='Success'))
+
+        # Create an SlaMiss where notification was sent, but email was not
+        session.merge(SlaMiss(task_id='dummy_deleted',
+                              dag_id='test_sla_miss',
+                              execution_date=test_start_date))
+
+        mock_log = mock.MagicMock()
+        dag_file_processor = DagFileProcessor(dag_ids=[], log=mock_log)
+        dag_file_processor.manage_slas(dag=dag, session=session)
+
     def test_dag_file_processor_dagrun_once(self):
         """
         Test if the dag file proccessor does not create multiple dagruns

markjrobert2/airflow

tests/jobs/test_scheduler_job.py
@@ -1939,6 +1939,37 @@ class SchedulerJobTest(unittest.TestCase):
 
         sla_callback.assert_not_called()
 
+    def test_dag_file_processor_sla_miss_deleted_task(self):
+        """
+        Test that the dag file processor will not crash when trying to send
+        sla miss notification for a deleted task
+        """
+        session = settings.Session()
+
+        test_start_date = days_ago(2)
+        dag = DAG(dag_id='test_sla_miss',
+                  default_args={'start_date': test_start_date,
+                                'sla': datetime.timedelta(days=1)})
+
+        task = DummyOperator(task_id='dummy',
+                             dag=dag,
+                             owner='airflow',
+                             email='test@test.com',
+                             sla=datetime.timedelta(hours=1))
+
+        session.merge(models.TaskInstance(task=task,
+                                          execution_date=test_start_date,
+                                          state='Success'))
+
+        # Create an SlaMiss where notification was sent, but email was not
+        session.merge(SlaMiss(task_id='dummy_deleted',
+                              dag_id='test_sla_miss',
+                              execution_date=test_start_date))
+
+        mock_log = mock.MagicMock()
+        scheduler = SchedulerJob(dag_ids=['test_sla_miss'], log=mock_log)
+        scheduler.manage_slas(dag=dag, session=session)
+
     def test_scheduler_executor_overflow(self):
         """
         Test that tasks that are set back to scheduled and removed from the executor